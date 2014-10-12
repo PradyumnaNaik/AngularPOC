3a4031fcaf236850807255feefdcc5206d237473
PradyumnaNaik <pradyumna.naik@gmail.com>
2014-10-12 19:30:47 +0530

    added comments to the exception service

--- app/scripts/models/services/exceptionHandlingService.js
+++ app/scripts/models/services/exceptionHandlingService.js
@@ -1,5 +1,9 @@
 angular.module('ExceptionHandler',[]).
 factory('exHandler',[function(){
+    /*
+        debug/release. 
+        This switch can be used to differentiate behavior in dev/production. would be very helpful in Hybrid app.
+    */
     var ex = {},
         isDebug = true;
     ex.logError = function(err){
